49f9aee6c0a1bacba884bff4aec69133e82489c4
mindthelab <65050803+mindthelab@users.noreply.github.com>
2024-01-10 07:03:03 -0300

    temp-converter v0.2
    - adição de mais comentários para facilitar a localização e entendimento do código
    - criação de um arquivo função para a simplificação do arquivo principal
    - mudanças menores por boas praticas de programação
    
    lembrando que todo o conhecimento de funcionalidades novas e boas praticas que puderem ser implementadas para a otimização do código, serão implementadas.

---
+++ func.h
@@ -0,0 +1,14 @@
+//cabeçalho da função
+#ifndef FUNC_H
+#define FUNC_H
+
+float ctof (float temperatura);
+float ctok (float temperatura);
+
+float ftoc (float temperatura);
+float ftok (float temperatura);
+
+float ktoc (float temperatura);
+float ktof (float temperatura);
+
+#endif
